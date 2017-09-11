f7c6df638086e1e2d45a5babdcd6846d4a250ad4
hezijian <2506596454@qq.com>
2017-09-11 14:14:18 +0800

    Update captcha_recognition.py

--- code/captcha_recognition.py
+++ code/captcha_recognition.py
@@ -49,8 +49,8 @@ if K.image_dim_ordering() == 'th':
     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols, 1)
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols,1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
 X_train = X_train.astype('float32')
